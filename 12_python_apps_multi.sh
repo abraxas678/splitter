702a3ab38fb2d28e26f21b18f1594e0dbab5b7dd
abraxas678 <abraxas678@gmail.com>
2024-07-01 00:08:35 +0200

    1

--- 12_python_apps_multi.sh
+++ 12_python_apps_multi.sh
@@ -2,8 +2,7 @@
 #9. Python3 install + Apps
 
 # Install Python packages using pipx
-installme python3-pip
-installme pipx
-pipx install rich-cli
-pipx install shell-gpt
-pipx install apprise
+
+  while IFS= read -r line; do
+    [[ $line != "#"* ]] && pipx install $line
+  done <  python_apps_all_multi.txt 
